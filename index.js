df67094106794ec80509a8a16dfddde047af2fee
Lucas <lkm027@gmail.com>
2018-07-15 14:07:57 -0400

    Adding login route

--- index.js
+++ index.js
@@ -27,24 +27,37 @@ const database = firebase.database();
 app.use( '/public', express.static( 'public/'  ) );
 app.use( express.static( 'public/', { extensions: ['html'] } ) );
 
+// Create a new user account
 app.post( '/create_new_account', function( req, res ) {
-    let email = req.body.email;
+    let email    = req.body.email;
     let password = req.body.password;
-
-    var failed = false;
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .then( function( success ) {
             safe_email = email.replace( '.', '_' );
             firebase.database().ref('users').child(safe_email).set({
-                email: email,
+                email : email,
+                student : true
             });
 
-            res.status(200).send( 'Congrats new user' );
+            res.status(200).send( { email : email } );
         }, function( error ) {
             console.log( error );
             res.status(500).send( error.message );
         });
+});
+
+// Login a user
+app.get( '/login_user', function( req, res ) {
+    let email    = req.query.email;
+    let password = req.query.password;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then( function( success ) {
+        res.status(200).send( { email : email } );
+    }, function( error ) {
+        res.status(500).send( error.message );
+    });
 });
 
 // Please keep this peice of middleware at the bottom of this page. It reroutes 404s.
